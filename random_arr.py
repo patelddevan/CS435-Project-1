a98c001391cb2f7aca9268d39004a2a67604e581
patelddevan <40066975+patelddevan@users.noreply.github.com>
2020-02-24 14:50:21 -0500

    Create random_arr.py

---
+++ random_arr.py
@@ -0,0 +1,28 @@
+import random
+
+def getRandomArray(n):
+  random_array = list()
+  for i in range(n):
+    random_int = getRandomNumber(n)
+    while random_int in random_array:
+      random_int = getRandomArray(n)
+    random_array.append(random_int)
+  return random_array 
+
+def getRandomNumber(n):
+  return random.randint(0, n*10)
+
+def getSortedArray(n):
+  sorted_array = list()
+  for i in range(n, 0, -1):
+    sorted_array.append(i)
+  return sorted_array
+
+
+
+def main():
+  print(getRandomArray(10))
+  print(getSortedArray(10))
+
+if __name__ == "__main__":
+  main()
